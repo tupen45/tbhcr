97f6ecf47b2a597b171be82789f0bd1ad673fe84
Tbhcr <43102977+tupen45@users.noreply.github.com>
2023-03-04 22:51:31 +0530

    erferfe
    ferger

---
+++ 22/oto22.js
@@ -0,0 +1,43 @@
+// function print(){
+//    // document.write("wish you all happy diwali");
+// let myp= new Promise(function (resolve,reject){
+//     resolve("success");
+//     reject("fail");
+// });
+// myp.then((value)=>{ console.log("sucees")})
+// .catch((error)=>{console.log("failser")});
+
+// return myp;
+// }
+
+
+// async function demo(){
+//     await print();
+    
+// }
+// demo();
+// var matrix= [[1,2,3,]
+// ,[4,5,6],
+// [7,8,9]
+// ];
+// for(let i=0;i<=2;i++){
+//     for(let j=0;j<=2;j++){
+//         document.write(" "+[i][j]+" ");
+
+//     }
+//     document.write("<br>");
+   
+//}
+// var myHeaders = new Headers();
+// myHeaders.append("apikey", "FvRUOsQY047WaycmmXOWnIBJ6c88b1tY");
+
+// var requestOptions = {
+//   method: 'GET',
+//   redirect: 'follow',
+//   headers: myHeaders
+// };
+
+// fetch("https://api.apilayer.com/spotify/album_metadata?id={id}", requestOptions)
+//   .then(response => response.text())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
